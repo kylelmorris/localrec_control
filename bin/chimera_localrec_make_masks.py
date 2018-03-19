b9f6bfb3218e304c34ab09be7b0708b63f8b4f23
kylelmorris <kylelmorris@gmail.com>
2018-03-19 11:08:58 -0700

    commit

--- bin/chimera_localrec_make_masks.py
+++ bin/chimera_localrec_make_masks.py
@@ -13,6 +13,26 @@
 # /PDB      # Contains fitted PDB's in the map
 #
 
+# Python modules for option import
+
+#import argparse
+
+#parser = argparse.ArgumentParser()
+#parser.add_argument("r", type=int, help="Density selection radius from C-alpha (A)")
+#parser.add_argument("--e", "--extend", help="--e Extend mask by n pixels",
+#                    type=int)
+#parser.add_argument("--s", "--soften", help="--s Soften mask by n pixels",
+#                    type=int)
+#args = parser.parse_args()
+
+#import argparse
+#parser = argparse.ArgumentParser()
+#parser.add_argument("square", type=int,
+                    #help="display a square of a given number")
+#parser.add_argument("-v", "--verbosity", type=int,
+#                    help="increase output verbosity")
+#args = parser.parse_args()
+
 #####################################################################################
 # Python module import, chimera command and variable setup
 #####################################################################################
@@ -80,10 +100,14 @@ addCommand('savemarkers', save_markers_command)
 #####################################################################################
 
 # Required variables
-radius = 20      # Radius around selection to extract density
-#origin = 'originIndex 150'    # Insert a volume origin command here if desired
-origin = ''    # Insert a volume origin command here if desired
+radius = 25            # Radius around selection to extract density
+#origin = 'originIndex 150'     # Insert a volume origin command here if desired
+origin = ''                     # Insert a volume origin command here if desired
 residuesel = '@ca'              # restrict residues mask extraction
+
+os.mkdir(str(dir)+'/masks')
+os.mkdir(str(dir)+'/images')
+os.mkdir(str(dir)+'/cmm_markers')
 
 #####################################################################################
 # Open map into #0
@@ -91,15 +115,11 @@ residuesel = '@ca'              # restrict residues mask extraction
 
 # Open map back into #0
 rc('open #0 '+str(dir)+'/map/'+str(map))
-rc('volume #0 '+str(origin))
+rc('volume #0 step 1 '+str(origin))
 
 #####################################################################################
 # Create and save masks by sequential PDB model opening
 #####################################################################################
-
-os.mkdir(str(dir)+'/masks')
-os.mkdir(str(dir)+'/images')
-os.mkdir(str(dir)+'/cmm_markers')
 
 # Loop through the PDB models to create the masks for subtraction
 for i in range(1,fileno+1):
@@ -114,26 +134,50 @@ for i in range(1,fileno+1):
   rc('~select #1')
   rc('~modeldisplay #1')
 
-  # Make cmm markers based on centre of mass
-  rc('measure center #0 mark true radius 10 color red model 2')
-  rc('measure center #3 mark true radius 10 color blue model 2')
-  rc('savemarkers #2 '+str(dir)+'/cmm_markers/marker'+str(i)+'.cmm')
+  # Make cmm markers based on centre of mass of map, now done based on PDB mass as above
+  #rc('measure center #0 mark true radius 10 color red model 2')
+  #rc('measure center #3 mark true radius 10 color blue model 2')
+  #rc('savemarkers #2 '+str(dir)+'/cmm_markers/marker'+str(i)+'.cmm')
 
   # save new volumes and image for diagnostic
   rc('focus')
   rc('volume # hide')
   rc('volume #2 show')
-  rc('volume #2 save '+str(dir)+'/masks/mask'+str(i)+'_subtraction.mrc')
+  rc('volume #2 save '+str(dir)+'/masks/mask'+str(i)+'_subtraction.mrc')    # used to be called subtraction.mrc
   rc('copy file '+str(dir)+'/images/mask'+str(i)+'_subtraction.png png')
 
   rc('set bgTransparency')
   rc('volume # hide')
   rc('volume #3 show')
-  rc('volume #3 save '+str(dir)+'/masks/mask'+str(i)+'_remaining_density.mrc')
-  rc('copy file '+str(dir)+'/images/mask'+str(i)+'_remaining_density.png png')
+  rc('volume #3 save '+str(dir)+'/masks/mask'+str(i)+'_subparticle.mrc')    # used to be called *_remaining density.mrc
+  rc('copy file '+str(dir)+'/images/mask'+str(i)+'_subparticle.png png')
 
   rc('volume # hide')
   rc('modeldisplay #1')
   rc('copy file '+str(dir)+'/images/mask'+str(i)+'_PDB.png png')
 
   rc('close #1-3')
+
+#####################################################################################
+# Create cmm markers based on center of mass in PDBs
+#####################################################################################
+
+rc('close #')
+
+# Loop through the PDB models to create the center of mass markers for subparticl location
+for i in range(1,fileno+1):
+  # Load PDBs
+  PDB = filelist[i-1]
+  rc('open #'+str(i)+' '+str(dir)+'/PDB/'+str(PDB))
+
+for i in range(1,fileno+1):
+  #measure cmm markers
+  rc('measure center # mark true radius 10 color red model 0')
+  rc('measure center #'+str(i)+' mark true radius 10 color blue model 0')
+  rc('savemarkers #0 "'+str(dir)+'/cmm_markers/marker'+str(i)+'.cmm"')
+  rc('close #0')
+
+rc('close #')
+
+#close chimera
+rc('stop')
--- localrec_create_masks.sh
+++ localrec_create_masks.sh
@@ -1,11 +1,55 @@
 #!/bin/bash
 #
 
-#User set variables
-export LOCALREC_SCRIPTS='/Users/lfsmbe/Dropbox/Scripts/github/localrec_control/bin'
-export CHIMERA_EXE='/Applications/Science/Chimera.app/Contents/MacOS/chimera'
-export ini_threshold='0.00283'
-export soften='9'
+####################################################################################
+#User VARIABLES
+#export LOCALREC_SCRIPTS='/Users/lfsmbe/Dropbox/Scripts/github/localrec_control/bin'
+#export CHIMERA_EXE='/Applications/Science/Chimera.app/Contents/MacOS/chimera'
+
+#export LOCALREC_SCRIPTS='/home/kmorris/Dropbox/Scripts/github/localrec_control/bin'
+#export CHIMERA_EXE='/usr/bin/chimera'
+
+#export LOCALREC_SCRIPTS=$(which localrec_create_masks.sh | sed 's/\ /\\ /g' | sed 's/(/\\(/g' | sed 's/)/\\)/g' | sed 's/\/localrec_create_masks.sh//g')
+#export LOCALREC_SCRIPTS=$(which localrec_create_masks.sh | sed 's/\/localrec_create_masks.sh//g')
+export LOCALREC_SCRIPTS=~/Dropbox/Scripts/github/localrec_control
+export CHIMERA_EXE=$(which chimera)
+
+ini_threshold=$1 # ini-threshold for use on remaining density to soften subtraction mask
+extend=$2
+soften=$3
+
+if [[ -z $1 ]] || [[ -z $2 ]] || [[ -z $3 ]] ; then
+
+  echo ""
+  echo "Variables empty, usage is localrec_create_masks.sh (1) (2) (3)"
+  echo ""
+  echo "(1) = Input volume threshold"
+  echo "(2) = Extend mask by n pixels (4)"
+  echo "(3) = Soften mask by n pixels (9)"
+  exit
+
+fi
+
+####################################################################################
+# Get organised
+rm -rf cmm_markers
+rm -rf images
+rm -rf masks
+rm -rf bin
+
+mkdir bin
+echo "Script location for copying: "${LOCALREC_SCRIPTS}
+scp -r ${LOCALREC_SCRIPTS}/bin/chimera_localrec_make_masks.py bin
+scp -r ${LOCALREC_SCRIPTS}/localrec_create_masks.sh bin
+scp -r ${LOCALREC_SCRIPTS}/localrec_create_subparticles.sh bin
+
+#Save these parameters to file for note taking
+printf "localrec_create_masks parameters\n" > localrec_create_masks.out
+printf "Input volume threshold: $ini_threshold\n" >> localrec_create_masks.out
+printf "Extend mask by n pixels: $extend\n" >> localrec_create_masks.out
+printf "Soften mask by n pixels: $soften\n" >> localrec_create_masks.out
+
+####################################################################################
 
 echo ''
 echo 'This script is designed to create masks for relion localized reconstruction...'
@@ -22,18 +66,12 @@ echo 'Note, existing cmm_marker, image and mask folders will be deleted'
 read p
 echo ''
 
-# Get organised
-scp -r ${LOCALREC_SCRIPTS}/chimera_localrec_make_masks.py .
-
-rm -rf cmm_markers
-rm -rf images
-rm -rf masks
-
 # Create initial masks
 echo 'Using UCSF Chimera to create initial masks selected by PDBs using scolor'
 echo ''
 
-${CHIMERA_EXE} ./chimera_localrec_make_masks.py
+scp -r bin/chimera_localrec_make_masks.py .
+${CHIMERA_EXE} chimera_localrec_make_masks.py ${radius}
 
 # Soften masks
 echo 'Softening masks for signal subtraction using Relion...'
@@ -45,24 +83,27 @@ for f in masks/*subtraction.mrc ; do
   file=${f%_subtraction.mrc}
   echo "Basename is $file"
   echo ''
-  remaining=$file"_remaining_density.mrc"
-  remain_soft_inv=$file"_remaining_density_soft_inv_mask.mrc"
+  remaining=$file"_subtraction.mrc"                 #used to be *remaining_density.mrc
+  remain_soft=$file"_subtraction_soft_mask.mrc"
   subtraction_soft=$file"_subtraction_soft.mrc"
 
-  #Soften remaining_density.mrc with relion_mask_create and invert
-  relion_mask_create --i ${remaining} --ini_threshold $ini_threshold --width_soft_edge $soften --invert --o ${remain_soft_inv}
+  #Soften subtraction.mrc with relion_mask_create
+  echo ""
+  echo ">>> relion_mask_create --i ${remaining} --ini_threshold $ini_threshold --extend_inimask $extend --width_soft_edge $soften --o ${remain_soft}"
+  relion_mask_create --i ${remaining} --ini_threshold $ini_threshold --extend_inimask $extend --width_soft_edge $soften --o ${remain_soft}
+  #Soften subtraction.mrc with relion_mask_create and invert
+  #relion_mask_create --i ${remaining} --ini_threshold $ini_threshold --width_soft_edge $soften --invert --o ${remain_soft}
 
   #Multiply inverted softened mask by subtraction.mrc mask to get soft density for subtraction
-  relion_image_handler --i $f --multiply ${remain_soft_inv} --o $subtraction_soft
+  echo ""
+  echo ">>> relion_image_handler --i map/*.mrc --multiply ${remain_soft} --o $subtraction_soft"
+  relion_image_handler --i map/*.mrc --multiply ${remain_soft} --o $subtraction_soft
 
+  echo ""
   echo "Created soft mask for signal subtraction: ${subtraction_soft}"
   echo ''
 
 done
-
-echo 'Cleaning up...'
-rm -rf chimera_localrec_make_masks.py
-rm -rf chimera_localrec_make_masks.pyc
 
 echo ''
 echo 'Done!'
